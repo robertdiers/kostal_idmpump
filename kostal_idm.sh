e1da32f1cb7c4b88604e8bbcca5ecf1997b5f3ab
Robert Diers <robertdiers@gmail.com>
2021-11-14 15:28:04 +0100

    use bash

--- kostal_idm.sh
+++ kostal_idm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source $HOME/.bash_profile
-echo $INVERTER_IP
+echo INVERTER_IP + $INVERTER_IP
+echo INVERTER_PORT + $INVERTER_PORT
+echo IDM_IP + $IDM_IP
+echo IDM_PORT + $IDM_PORT
+echo FEED_IN_LIMIT + $FEED_IN_LIMIT
 python3 /app/kostal_idm.py
