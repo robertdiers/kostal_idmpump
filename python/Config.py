0206aff84bd03cb221f114fa453b7c95befb2f8a
Robert Diers <robertdiers@gmail.com>
2023-01-25 19:37:08 +0100

    Update Config.py

--- python/Config.py
+++ python/Config.py
@@ -19,22 +19,22 @@ def read():
         values["feed_in_limit"] = int(config['IdmSection']['feed_in_limit']) 
         if os.getenv('IDM_IP','None') != 'None':
             values["idm_ip"] = os.getenv('IDM_IP')
-            print ("using env: IDM_IP")
+            #print ("using env: IDM_IP")
         if os.getenv('IDM_PORT','None') != 'None':
             values["idm_port"] = int(os.getenv('IDM_PORT'))
-            print ("using env: IDM_PORT")
+            #print ("using env: IDM_PORT")
         if os.getenv('FEED_IN_LIMIT','None') != 'None':
             values["feed_in_limit"] = int(os.getenv('FEED_IN_LIMIT'))
-            print ("using env: FEED_IN_LIMIT")
+            #print ("using env: FEED_IN_LIMIT")
 
         values["inverter_ip"] = config['KostalSection']['inverter_ip']
         values["inverter_port"] = int(config['KostalSection']['inverter_port'])
         if os.getenv('INVERTER_IP','None') != 'None':
             values["inverter_ip"] = os.getenv('INVERTER_IP')
-            print ("using env: INVERTER_IP")
+            #print ("using env: INVERTER_IP")
         if os.getenv('INVERTER_PORT','None') != 'None':
             values["inverter_port"] = int(os.getenv('INVERTER_PORT'))
-            print ("using env: INVERTER_PORT")
+            #print ("using env: INVERTER_PORT")
         
         #print (datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " config: ", values)
 
